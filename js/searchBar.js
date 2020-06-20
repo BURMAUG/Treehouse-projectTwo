ce62eeaa53178b07c51c687b2ac8ac995c9dfaa9
Deejai <burmaud1@gmail.com>
2020-06-20 08:45:32 -0400

    Buggy two

--- js/searchBar.js
+++ js/searchBar.js
@@ -16,10 +16,10 @@ searchDiv.appendChild(input);
 searchDiv.appendChild(searchButton);
 
 
-//lets get the collection of h3 content 
+//lets get the collection of h3 content
 const hThree = document.getElementsByTagName('h3');
 const list = document.getElementsByClassName("student-item cf");
-var i =0;
+var i = 0;
 var student_list = [];
 
 function searching(searchInput, names) {
@@ -41,19 +41,24 @@ function searching(searchInput, names) {
       list[i].style.display = "none";
     }
   }
-  showPage(student_list, 1);
-  appendPageLinks(student_list);
-}
+};
 
 //buttons
 searchButton.addEventListener("click", (e) => {
   e.preventDefault();
-  searching(input, hThree);
+  //searching(input.value, hThree);
+  console.log(e.target.textContent);
   if (input.value === ''){
       alert('Place putting a valid search input');
   }
-  input.value = "";
+  searching(input, hThree);//searching for the intended person
+
+  showPage(student_list, 1);//showing the page 
+  appendPageLinks(student_list);//appending how many paginations needed
+  input.value = ''; //changing the search input space to nothing.
 });
+
+
 
 //input value
 input.addEventListener('keyup', (event)=>{
@@ -62,7 +67,7 @@ input.addEventListener('keyup', (event)=>{
     //console.log(target);
 
     //loop throught the name and the check
-    //like to check if the target value is in any of the name 
+    //like to check if the target value is in any of the name
     //i have learn
     for(var j =0; j<hThree.length; j++){
         if(hThree[j].textContent.toLowerCase().includes(target.toLowerCase())){
@@ -72,7 +77,10 @@ input.addEventListener('keyup', (event)=>{
             list[j].style.display = 'none';
         }
     }
-    appendPageLinks(student_list);
-    showPage(student_list, 1);
-    
+    // appendPageLinks(student_list);
+    // showPage(student_list, 1);
+
 });
+ 
+appendPageLinks(student_list);
+showPage(student_list, 1);
